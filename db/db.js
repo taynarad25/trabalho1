0467c68ef474bc03b121459b57b147a5b99029fc
taynarad25 <taynarad25@alu.ufc.br>
2022-11-30 19:27:55 -0300

    Trabalho Web

---
+++ db/db.js
@@ -0,0 +1,68 @@
+async function connect(){
+    if(global.connection && global.connection.state !== 'disconnected')
+        return global.connection;
+
+    const mysql = require("mysql2/promise");
+    const connection = await mysql.createConnection("mysql://root:123456@localhost:3306/DB_SERVICOS");
+    console.log("Conectou no MySQL!");
+    global.connection = connection;
+    return connection;
+}
+
+async function selectTipo(){
+    const conn = await connect();
+    const [rows] = await conn.query('select * from  Tipo;');
+    return rows;
+}
+async function selectServicos(){
+    const conn = await connect();
+    const [rows] = await conn.query('select * from  Servicos;');
+    return rows;
+}
+
+async function selectInformacoes(){
+    const conn = await connect();
+    const [rows] = await conn.query('select * from  Informacoes;');
+    return rows;
+}
+
+async function insertTipo(tipo){
+    const conn = await connect();
+    const sql = 'INSERT INTO TIPO(Nome) VALUE (?);';
+    const values = [tipo.nome];
+    return await conn.query(sql, values);
+}
+
+async function insertServicos(servico){
+    const conn = await connect();
+    const sql = 'INSERT INTO Servicos(Id_Tipo, Nome) VALUE (?, ?);';
+    const values = [servico.idTipo, servico.nome]
+    return await conn.query(sql, values);
+}
+
+async function insertInformacoes(informacoes){
+    const conn = await connect();
+    const sql = 'INSERT INTO Informacoes(Id_Servicos, Id_Tipo, Nome, Horario, Endereco, Telefone, Instagram, Whatsapp) VALUE (?, ?, ?, ?, ?, ?, ?, ?);';
+    const values = [informacoes.idTipo, informacoes.idServico, informacoes.nome, informacoes.horario, informacoes.endereco, informacoes.telefone, informacoes.instagram, informacoes.whatsapp]
+    return await conn.query(sql, values);
+}
+
+async function deleteTipo(idTipo){
+    const conn = await connect();
+    const sql = 'DELETE FROM Tipo where Id_Tipo = (?)';
+    return await conn.query(sql, [idTipo]);
+}
+
+async function deleteServicos(idServicos){
+    const conn = await connect();
+    const sql = 'DELETE FROM Servicos where Id_Servicos = (?)';
+    return await conn.query(sql, [idServicos]);
+}
+
+async function deleteInformacoes(idInformacoes){
+    const conn = await connect();
+    const sql = 'DELETE FROM Informacoes where Id_Informacoes = (?)';
+    return await conn.query(sql, [idInformacoes]);
+}
+
+module.exports = {selectTipo, selectServicos, selectInformacoes, insertTipo, insertServicos, insertInformacoes, deleteTipo, deleteServicos, deleteInformacoes}
---
+++ server/index.js
@@ -0,0 +1,85 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const db = require('../db/db.js');
+
+
+
+app.get('/tipo', async (request, response) => {
+  const {} = request.query;
+  const result = await db.selectTipo();
+  return response.json(result);
+})
+
+app.get('/servicos', async (request, response) => {
+  const {} = request.query;
+  const result = await db.selectServicos();
+  console.log(result)
+  return response.json(result);
+})
+
+app.get('/informacoes', async (request, response) => {
+  const {} = request.query;
+  const result = await db.selectInformacoes();
+  console.log(result)
+  return response.json(result);
+})
+
+app.post('/tipo', async (request, response) => {
+  const {nome} = request.body
+  const novoTipo = {
+    nome
+  }
+  await db.insertTipo(novoTipo)
+
+  return response.status(201).json(novoTipo);
+})
+app.post('/servicos', async (request, response) => {
+  const {idTipo, nome} = request.body
+  const novoServico = {
+    idTipo,
+    nome
+  }
+  await db.insertServicos(novoServico)
+
+  return response.status(201).json(novoServico);
+})
+app.post('/informacoes', async (request, response) => {
+  const {idTipo, idServico, nome, horario, endereco, telefone, instagram, whatsapp} = request.body
+  const novoInformacoes = {
+    idTipo,
+    idServico,
+    nome,
+    horario,
+    endereco,
+    telefone,
+    instagram,
+    whatsapp
+  }
+  await db.insertInformacoes(novoInformacoes)
+
+  return response.status(201).json(novoInformacoes);
+})
+
+app.delete('/tipo/:id', async (request, response) => {
+  const {id} = request.params;
+  const result = await db.deleteTipo(id)
+  return response.json(result)
+})
+app.delete('/servicos/:id', async (request, response) => {
+  const {id} = request.params;
+  const result = await db.deleteServicos(id)
+  return response.json(result)
+})
+app.delete('/informacoes/:id', async (request, response) => {
+  const {id} = request.params;
+  const result = await db.deleteInformacoes(id)
+  return response.json(result)
+})
+
+app.listen(8080,() =>{
+  console.log("Servidor funcionando!!!")
+})
